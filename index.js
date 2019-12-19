50850e39f29444e4103c39184c9e2af9101ac1fb
shraavyaanasuri <44354520+shraavyaanasuri@users.noreply.github.com>
2019-12-19 13:57:01 +0530

    modifiedpageheroku

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT|| 3000
-
-app.get('/', (req, res) => res.send('Hello World from Rajesh!'))
-
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/page1.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
